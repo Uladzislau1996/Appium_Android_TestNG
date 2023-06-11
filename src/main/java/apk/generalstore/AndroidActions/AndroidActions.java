d2e9a1140bcc9a48b66b7938bc32500f6252092a
Vladislav Galushkevich <mr.halushkevich@gmail.com>
2023-06-11 16:36:24 +0400

    Remove unused methods

--- src/main/java/apk/generalstore/AndroidActions/AndroidActions.java
+++ src/main/java/apk/generalstore/AndroidActions/AndroidActions.java
@@ -19,18 +19,7 @@ public class AndroidActions {
         ((JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of(
                 "elementId", ((RemoteWebElement) element).getId(), "duration", 2000));
     }
-
-    public void scrollToEnd() {
-        boolean canScrollMore;
-        do {
-            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
-                    "left", 100, "top", 100, "width", 200, "height", 200,
-                    "direction", "down",
-                    "percent", 3.0
-            ));
-        } while (canScrollMore);
-    }
-
+    
     public void scrollToText(String text) {
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()" +
                 ".scrollable(true).instance(0)).scrollIntoView(new UiSelector()." +
@@ -40,14 +29,6 @@ public class AndroidActions {
     public Double getPrice(String itemPrice) {
         Double price = Double.parseDouble(itemPrice.substring(1));
         return price;
-    }
-
-    public void swipeAction(WebElement element, String direction) {
-        ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
-                "elementId", ((RemoteWebElement) element).getId(),
-                "direction", direction,
-                "percent", 0.75
-        ));
     }
 
 }
